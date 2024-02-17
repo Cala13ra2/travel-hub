9f9d278c5d947a244f5ccb9f346786b5d33c9f2c
iro claire <iroclairec@gmail.com>
2024-02-17 14:39:54 +0100

    first

---
+++ script/book.js
@@ -0,0 +1,7 @@
+// const sliders = document.getElementById("slider");
+// const result = document.getElementById("result");
+// result.innerText = slider.value;
+
+// slider.addEventListener("input", () => {
+//   result.innerText = slider.value;
+// });
